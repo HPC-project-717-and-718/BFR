2510c9498a99a90f962efdba57748dfe51bdc125
Luca <luca.cavalcanti@studenti.unitn.it>
2023-11-02 14:14:18 +0100

    TODO: add a way to automatically create the clusters, as now they have to be inserted manually

--- utils/plot_data.py
+++ utils/plot_data.py
@@ -17,9 +17,9 @@ dimension_axis = [[] for _ in range(dimensions)]
 
 # transform points to float
 for i in range(len(points)):
-    points[i] = points[i].split("\t")
+    points[i] = points[i].split(" ")
     # remove element containing \n
-    points[i].pop()
+    # points[i].pop()
     for j in range(dimensions):
         points[i][j] = float(points[i][j])
         dimension_axis[j].append(points[i][j])
@@ -30,27 +30,33 @@ fig, ax = plt.subplots()
 
 plt.scatter(dimension_axis[0], dimension_axis[1], marker='o', color='b', s=0.5, label='Data Points')
 
+# clusters = {
+#     0: {'centroid': (-0.019072, 2.024158), 'count': 1573, 'variance': [4000/1573 - (-30/1573)**2, 13326/1573 - (3184/1573)**2]},
+#     # 1: {'centroid': (4.291720, 0.336306), 'count': 785, 'variance': [69720/785 - (3369/785)**2, 4250/785 - (264/785)**2]},
+#     2: {'centroid': (3.318966, 4.909483), 'count': 232, 'variance': [4004/232 - (770/232)**2, 6906/232 - (1139/232)**2]},
+#     # 3: {'centroid': (380.500000, 0.000000), 'count': 6, 'variance': [1361517/6 - (2283/6)**2, 73/6 - (0/6)**2]},
+#     4: {'centroid': (-2.927326, 2.750000), 'count': 344, 'variance': [4261/344 - (-1007/344)**2, 4938/344 - (946/344)**2]},
+#     5: {'centroid': (-2.596774, -0.489247), 'count': 186, 'variance': [2185/186 - (-483/186)**2, 579/186 - (-91/186)**2]},
+#     6: {'centroid': (2.365854, -2.506098), 'count': 656, 'variance': [6837/656 - (1552/656)**2, 6312/656 - (-1644/656)**2]},
+#     # 7: {'centroid': (8937066.000000, 3.000000), 'count': 2, 'variance': [-2147483648/2 - (17874132/2)**2, 21/2 - (6/2)**2]},
+#     # 8: {'centroid': (153329.333333, -0.666667), 'count': 3, 'variance': [-2147483648/3 - (459988/3)**2, 4/3 - (-2/3)**2]},
+#     # 7: {'centroid': (6131.750000, 0.750000), 'count': 4, 'variance': [345379178/4 - (24527/4)**2, 34/4 - (3/4)**2]}
+# }
 
-clusters = {
-    0: {'centroid': [2.350181, 3.919374], 'count': 831, 'variance': [9707/831 - (1953/831)**2, 16560/831 - (3257/831)**2]},
-    1: {'centroid': [5.115044, 2.592920], 'count': 113, 'variance': [3576/113 - (578/113)**2, 1094/113 - (293/113)**2]},
-    2: {'centroid': [1.703704, 1.386831], 'count': 243, 'variance': [1574/243 - (414/243)**2, 934/243 - (337/243)**2]},
-    8: {'centroid': [-0.834615, 1.671795], 'count': 1560, 'variance': [4057/1560 - (-1302/1560)**2, 9948/1560 - (2608/1560)**2]},
-    9: {'centroid': [-0.840909, 5.954545], 'count': 44, 'variance': [73/44 - (-37/44)**2, 1786/44 - (262/44)**2]}
-}
+# # Define a list of colors
+# colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'orange', 'purple', 'lime']
 
-# Define a list of colors
-colors = ['r', 'g', 'b', 'c', 'm']
-
-# Create plot and add ellipses with different colors
-for i, cluster in clusters.items():
-    std_deviation_x = math.sqrt(cluster['variance'][0])
-    std_deviation_y = math.sqrt(cluster['variance'][1])
-    ellipse = patches.Ellipse(cluster['centroid'], std_deviation_x*3, std_deviation_y*3, alpha=0.5, label=f'Cluster {i}', color=colors[i % len(colors)])
-    ax.add_patch(ellipse)
+# # Create plot and add ellipses with different colors
+# for i, cluster in clusters.items():
+#     std_deviation_x = math.sqrt(abs(cluster['variance'][0]))
+#     std_deviation_y = math.sqrt(abs(cluster['variance'][1]))
+#     ellipse = patches.Ellipse(cluster['centroid'], std_deviation_x*3, std_deviation_y*3, alpha=0.5, label=f'Cluster {i}', color=colors[i % len(colors)])
+#     ax.add_patch(ellipse)
 
 # Set aspect to equal to ensure circular shape
-# ax.set_aspect('equal', adjustable='box')
+ax.set_aspect('equal', adjustable='box')
+
+# Adjust the x and y axis limits
 padding = 1.0  # adjust as needed
 plt.xlim(min(dimension_axis[0]) - padding, max(dimension_axis[0]) + padding)
 plt.ylim(min(dimension_axis[1]) - padding, max(dimension_axis[1]) + padding)
